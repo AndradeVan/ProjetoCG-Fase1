9fc397b01bb81bc71c41a1cd5357dc7f61f6ce76
vander_lei_andrade@outlook.com <vander_lei_andrade@outlook.com>
2019-07-08 19:42:22 -0300

    atualização

--- objeto.js
+++ objeto.js
@@ -5,217 +5,147 @@ var renderer;
 var angle = 0;
 var radius = 100;
 var flagAprox = 0;
-var precisaoBezier = 50;
+var controls = {};
+var player = {height:1.8, speed: 2.5, turnSpeed: Math.PI * 0.02};
+var animation;
 
-var psyduck = new THREE.Object3D();
-var lamp = new THREE.Object3D();
-
-var cameraSelector = false;
-
-var points;
 
 var init = function() {
 
     /*criando cenario */
     scene = new THREE.Scene();
-    scene.background = new THREE.Color(0xffffff);
+    scene.background = new THREE.Color(0x77dd77);
+    /*parte do fundo branco*/
+    scene.fog = new THREE.Fog( 0xffffff, 1000, 4000 );
+    
 
     /*inicializando camera1 e camera2 */
-    camera1 = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    camera1.position.z = 200;
-    camera1.position.y = 20;
+   camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 4000);
+   camera.position.set(0, 150, 1300);
+   camera.lookAt(new THREE.Vector3(0,player.height,0));  
 
-    camera2 = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-    camera2.position.z = 200;
-    camera2.position.y = 20;
+   scene.add(camera); 
 
-    var curve = new THREE.CubicBezierCurve3(
-      new THREE.Vector3( -10, 0, 0 ),
-      new THREE.Vector3( 100, 15, 0 ),
-      new THREE.Vector3( -15, 150, 0 ),
-      new THREE.Vector3( -15, 0, 0 )
-   );
+
+   scene.add( new THREE.AmbientLight( 0x222222 ) );
+
+	var light = new THREE.DirectionalLight( 0xffffff, 2.25 );
+	light.position.set( 200, 450, 500 );
+	light.castShadow = true;
+	light.shadow.mapSize.width = 1024;
+	light.shadow.mapSize.height = 512;
+	light.shadow.camera.near = 100;
+	light.shadow.camera.far = 1200;
+	light.shadow.camera.left = - 1000;
+	light.shadow.camera.right = 1000;
+	light.shadow.camera.top = 350;
+	light.shadow.camera.bottom = - 350;
+	scene.add( light );
+	
    
-   points = curve.getPoints(precisaoBezier);
-   var geometry = new THREE.BufferGeometry().setFromPoints( points );
-   var material = new THREE.LineBasicMaterial( { color : 0xff0000 } );
-   
-   // Create the final object to add to the scene
-   var curveObject = new THREE.Line( geometry, material );
+   var gt = new THREE.TextureLoader().load("textures/terrain/grasslight-big.jpg");
+	var gg = new THREE.PlaneBufferGeometry( 16000, 16000 );
+	var gm = new THREE.MeshPhongMaterial( { color: 0xffffff, map: gt } );
+	var ground = new THREE.Mesh(gg, gm) ;
+	ground.rotation.x = - Math.PI / 2;
+	ground.material.map.repeat.set( 64, 64 );
+	ground.material.map.wrapS = THREE.RepeatWrapping;
+	ground.material.map.wrapT = THREE.RepeatWrapping;
+	ground.receiveShadow = true;
+	scene.add(ground);
 
-   scene.add(curveObject);
-
-   document.addEventListener("keydown", movement);
-
-    /* ilumi*/
-	var ambientLight = new THREE.AmbientLight(0xcccccc,0.4);
-	scene.add(ambientLight);
-	var pointLight = new THREE.PointLight(0xffffff,0.8);
-	camera1.add(pointLight);
-	scene.add(camera1);
+   document.addEventListener("keydown", keydown);
+   document.addEventListener("keyup", keyup);
 
    /*criando os objetos */
    this.createObj();
-   this.createObj1();
 
-   /*O renderizador utilizado é o WebGL*/
+
    renderer = new THREE.WebGLRenderer();
    renderer.setPixelRatio(window.devicePixelRatio);
    renderer.setSize(window.innerWidth, window.innerHeight);
    document.body.appendChild(renderer.domElement);
+   renderer.gammaInput = true;
+	renderer.gammaOutput = true;
 
-   this.createPlane();
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-   this.render();
+
+   this.animate();
 };
+
+
 
 window.onload = this.init;
 
-/*funcao para mover o objeto Obj1 e mudar de posicao a camera */
-function movement(event) {
-   //var matrixRotationW = new THREE.Matrix4().makeRotationY(1,5708);
 
-    key = String.fromCharCode(event.which);
-
-   if (key == "W"){
-      psyduck.position.x = psyduck.position.x - 1;
-   } else if (key == "S"){
-      psyduck.position.x = psyduck.position.x + 1;
-   } else if (key == "D"){
-      psyduck.position.z = psyduck.position.z - 1;
-   } else if (key == "A"){
-      psyduck.position.z = psyduck.position.z + 1;
-   }
-
-   if (key == "1"){
-      cameraSelector = true;
-   } else if (key == "2"){
-      cameraSelector = false;
-   }
-
-};
-
-var i_curva = 0;
-var moverPatoBezier = function(points){
-  // var i;
-
-      //for(let i = 0; i <= precisaoBezier; i+=1){
-         psyduck.position.x = points[i_curva].x;
-         psyduck.position.y = points[i_curva].y;
-         psyduck.position.z = points[i_curva].z;
-        // sleep(500);
-      //}
-   i_curva+=1;
-}
 
 /*Criação do objeto 1 */
 var createObj = function(){
-    THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
-    var loader = new THREE.MTLLoader();
-    loader.load('Street_Lamp/StreetLamp.mtl', function (materials) {
+   loader = new THREE.AssimpLoader();
+	loader.load('models/assimp/octaminator/Octaminator.assimp', function (result) {
 
-        materials.preload();
-
-        var objloader = new THREE.OBJLoader();
-        objloader.setMaterials(materials)
-
-        objloader.load('Street_Lamp/StreetLamp.obj', function (object) {
-         var matrix = new THREE.Matrix4().makeScale(2, 2, 2);
-         object.applyMatrix(matrix);
-
-         lamp.add(object);
-			scene.add(lamp);
-        });
-    });
-}
-
-/*Criação do objeto 2 */
-var createObj1 = function(points){
-    THREE.Loader.Handlers.add(/\.dds$/i, new THREE.DDSLoader());
-    var loader = new THREE.MTLLoader();
-    loader.load('psyduck/psyduck.mtl', function (materials) {
-
-        materials.preload();
-
-        var objloader = new THREE.OBJLoader();
-        objloader.setMaterials(materials)
-
-        objloader.load('psyduck/psyduck.obj', function (object) {
-
-            object.rotation.z = 90 * Math.cos(0);
-            object.rotation.x = 45 * Math.cos(180);
-
-            var matrix = new THREE.Matrix4().makeTranslation(-100, 0, 0);
-            object.applyMatrix(matrix);
-
-            var matrixS = new THREE.Matrix4().makeScale(0.03, 0.03, 0.03);
-            object.applyMatrix(matrixS);
-
-			psyduck.add(object);
-         scene.add(psyduck);
-        });
-    });
-}
-
-var i_render = 0;
-var render = function() {
-    requestAnimationFrame(render);
-
-   if (cameraSelector){
-      this.camera1Position();
-      renderer.render (scene, camera1);
-   }
-   else{
-      this.camera2Position();
-      renderer.render (scene, camera2);
-   }
-
-    if(i_render == 0){
-        this.moverPatoBezier(points);
-    }
-    if(i_render == 1)
-        i_render = -1;
-    
-    i_render+=1;
-    
-  
+      object = result.object;
+      object.position.y = 250;
+      scene.add(object) ;
+      animation = result.animation;
+   }); 
+   
 };
 
-/*Criar um plano embaixo do objeto*/
-var createPlane = function() {
-    var planeGeometry = new THREE.PlaneGeometry(100, 100);
-    var planeMaterial = new THREE.MeshBasicMaterial({ color: 0xcccccc});
-    plane = new THREE.Mesh(planeGeometry, planeMaterial);
+var keydown = function(event){
+   controls[event.keyCode] = true;
+}
 
-    plane.rotation.x = -0.5 * Math.PI;
-    plane.position.y = -1;
-    scene.add(plane);
+var keyup = function(event){
+   controls[event.keyCode] = false;
+}
+
+var animate = function(){
+   requestAnimationFrame( animate, renderer.domElement );
+   
+
+   if(controls[87]){
+      object.position.x += Math.sin(object.rotation.y) * player.speed;
+      object.position.z += -Math.cos(object.rotation.y) * player.speed;
+   }
+   if(controls[83]){
+      object.position.x -= Math.sin(object.rotation.y) * player.speed;
+      object.position.z -= -Math.cos(object.rotation.y) * player.speed;
+   }
+   if(controls[65]){
+      object.position.x += Math.sin(object.rotation.y - Math.PI/2) * player.speed;
+      object.position.z += -Math.cos(object.rotation.y - Math.PI/2) * player.speed;
+   }
+   if(controls[68]){
+      object.position.x += Math.sin(object.rotation.y + Math.PI/2) * player.speed;
+      object.position.z += -Math.cos(object.rotation.y + Math.PI/2) * player.speed;
+   }
+
+   if(controls[37]){
+      object.rotation.y += player.turnSpeed; 
+   }
+
+   if(controls[39]){
+      object.rotation.y -= player.turnSpeed; 
+   }
+   //mover objeto
+   /*object.position.set(
+      camera.position.x - Math.sin(camera.rotation.y),
+      camera.position.y + 120,
+      camera.position.z - 350
+   );
+   object.rotation.set(
+      camera.rotation.x,
+      camera.rotation.y,
+      camera.rotation.z
+   );*/
+
+	renderer.render( scene, camera );
+	if ( animation ) animation.setTime( performance.now() / 1000 );
 };
 
-/*posicionar a camera 1*/
-var camera1Position = function() {
 
-    camera1.lookAt(scene.position);
-    camera1.position.x = radius * Math.cos(angle);
-    camera1.position.z = radius * Math.sin(angle);
-    camera1.position.y = 80;
-    //angle -= 0.01;
-}
-/*posicionar a camera 2*/
-var camera2Position = function() {
 
-    camera2.lookAt(scene.position);
-    camera2.position.x = radius * Math.cos(angle);
-    camera2.position.z = radius * Math.sin(20);
-    camera2.position.y = 130;
-    //angle += 0.01;
-}
 
-function sleep(milliseconds) {
-   var start = new Date().getTime();
-   for (var i = 0; i < 1e7; i++) {
-     if ((new Date().getTime() - start) > milliseconds){
-       break;
-     }
-   }
-}
