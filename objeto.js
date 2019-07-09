429603c0e15f2f8ebce8a0aca730a1be2a4bb32d
vander_lei_andrade@outlook.com <vander_lei_andrade@outlook.com>
2019-07-08 22:34:29 -0300

    caixas

--- objeto.js
+++ objeto.js
@@ -1,33 +1,38 @@
 var scene;
-var camera1, camera2;
+var camera, camera1;
 var renderer;
+var criarTextura;
 
+
+var create,create1,create2;
 var angle = 0;
 var radius = 100;
 var flagAprox = 0;
 var controls = {};
 var player = {height:1.8, speed: 2.5, turnSpeed: Math.PI * 0.02};
 var animation;
+var colicao = [];
 var clock;
 
 
 var init = function() {
 
-    /*criando cenario */
-    scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x77dd77);
-    /*parte do fundo branco*/
-    scene.fog = new THREE.Fog( 0xffffff, 1000, 4000 );
-    clock = new THREE.Clock();
+   /*criando cenario */
+   scene = new THREE.Scene();
+   scene.background = new THREE.Color(0x77dd77);
+   /*parte do fundo branco*/
+   scene.fog = new THREE.Fog( 0xffffff, 1000, 4000 );
+   clock = new THREE.Clock();
 
     /*inicializando camera1 e camera2 */
    camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 4000);
    camera.position.set(0,350,1300);
 	camera.lookAt(scene.position); 
-
    scene.add(camera); 
 
-
+   camera1 = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 4000);
+   scene.add(camera1);
+   
    scene.add( new THREE.AmbientLight( 0x222222 ) );
 
 	var light = new THREE.DirectionalLight( 0xffffff, 2.25 );
@@ -42,7 +47,30 @@ var init = function() {
 	light.shadow.camera.top = 350;
 	light.shadow.camera.bottom = - 350;
 	scene.add( light );
-	
+   
+   
+   create = new THREE.Mesh(
+      new THREE.BoxGeometry(400,400,400),
+      new THREE.MeshPhongMaterial({color:0xffffff})
+   );
+   scene.add(create);
+   create.position.set(0,250,-1300);
+   colicao.push(create);
+
+   var textura = new THREE.TextureLoader();
+   criarTextura = new TextureLoader.load("")
+
+   create1 = new THREE.Mesh(
+      new THREE.BoxGeometry(400,400,400),
+      new THREE.MeshPhongMaterial({color:0xffffff})
+   );
+   scene.add(create1);
+   create1.position.set(1000,250,-1300);
+   colicao.push(create1);
+
+
+
+
    
    var gt = new THREE.TextureLoader().load("textures/terrain/grasslight-big.jpg");
 	var gg = new THREE.PlaneBufferGeometry( 16000, 16000 );
@@ -72,15 +100,10 @@ var init = function() {
 	renderer.shadowMap.enabled = true;
 	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 
-
    this.animate();
 };
 
-
-
 window.onload = this.init;
-
-
 
 /*Criação do objeto 1 */
 var createObj = function(){
@@ -105,25 +128,34 @@ var keyup = function(event){
 
 var animate = function(){
    requestAnimationFrame( animate, renderer.domElement );
-   
+
+   render();
+   moviment();
+
+};
+var render = function(){
+
+   renderer.render( scene, camera );
+	if ( animation ) animation.setTime( performance.now() / 1000 );
+}
+var moviment = function(){
+
    var delta = clock.getDelta(); 
 	var moveDistance = 200 * delta; 
 	var rotateAngle = Math.PI / 2 * delta; 
 
-
-   /*W*/
    if(controls[87]){
       object.translateZ( -moveDistance );
    }
-   /*S*/
+   
    if(controls[83]){
       object.translateZ(  moveDistance );
    }
-   /*A*/
+   
    if(controls[65]){
       object.translateX( -moveDistance );
    }
-   /*D*/
+   
    if(controls[68]){
       object.translateX(  moveDistance );	
    }
@@ -141,12 +173,18 @@ var animate = function(){
 	camera.position.x = cameraOffset.x;
 	camera.position.y = cameraOffset.y;
 	camera.position.z = cameraOffset.z;
-	camera.lookAt( object.position );
+   camera.lookAt( object.position );
+   
 
+   /*if(controls[31]){
+      object.rotateOnAxis( new THREE.Vector3(0,1,0), rotateAngle);
+   }
 
-	renderer.render( scene, camera );
-	if ( animation ) animation.setTime( performance.now() / 1000 );
-};
+   if(controls[32]){
+      object.rotateOnAxis( new THREE.Vector3(0,1,0), -rotateAngle);
+   }*/
+
+}
 
 
 
